2fee81b13aba8980b0b46985f7ce121af6f2116e
Mitch Ball <mitchb145@gmail.com>
2016-10-15 19:51:13 +1100

    Fixed firebase code adding new note functionality

--- js/app.js
+++ js/app.js
@@ -3,53 +3,55 @@ app.controller("MainController", function($scope){
   // Misc variable initializations
   $scope.page = 'main'
   $scope.displayNote = 0;
-  $scope.notes = {
-    0: {
-      id: 0,
-      doctor: {
-        type: 'Neurologist',
-        name: 'Mitch Ball',
-        address: 'Fake Address, Address St.'
-      },
-      date:{
-        day: '14th',
-        month: 'Oct',
-        time: '2pm'
-      },
-      comments: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      prescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-    },
-    1: {
-      id: 1,
-      doctor: {
-        type: 'Cardiac Surgeon',
-        name: 'Alexander Jones',
-        address: '3 Melbourne Ave.'
-      },
-      date:{
-        day: '2nd',
-        month: 'Dec',
-        time: '9am'
-      },
-      comments: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      prescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-    },
-    2: {
-      id: 2,
-      doctor: {
-        type: 'Anaesthesiologist',
-        name: 'Fake Name',
-        address: '3 Territory Ave.'
-      },
-      date:{
-        day: '2nd',
-        month: 'Dec',
-        time: '9am'
-      },
-      comments: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      prescription:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-    }
-  }
+
+  // Temporary fake data
+  // $scope.notes = {
+  //   0: {
+  //     id: 0,
+  //     doctor: {
+  //       type: 'Neurologist',
+  //       name: 'Mitch Ball',
+  //       address: 'Fake Address, Address St.'
+  //     },
+  //     date:{
+  //       day: '14th',
+  //       month: 'Oct',
+  //       time: '2pm'
+  //     },
+  //     comments: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  //     prescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  //   },
+  //   1: {
+  //     id: 1,
+  //     doctor: {
+  //       type: 'Cardiac Surgeon',
+  //       name: 'Alexander Jones',
+  //       address: '3 Melbourne Ave.'
+  //     },
+  //     date:{
+  //       day: '2nd',
+  //       month: 'Dec',
+  //       time: '9am'
+  //     },
+  //     comments: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  //     prescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  //   },
+  //   2: {
+  //     id: 2,
+  //     doctor: {
+  //       type: 'Anaesthesiologist',
+  //       name: 'Fake Name',
+  //       address: '3 Territory Ave.'
+  //     },
+  //     date:{
+  //       day: '2nd',
+  //       month: 'Dec',
+  //       time: '9am'
+  //     },
+  //     comments: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  //     prescription:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  //   }
+  // }
 
   // Initialize Firebase
   var config = {
@@ -74,7 +76,7 @@ app.controller("MainController", function($scope){
   function writeNewNote(type, name, address, day, month, time, comments, prescription) {
 
     // Get a key for a new Post.
-    var newPostKey = database.ref().child('posts').push().key;
+    var newPostKey = database.ref().child('notes').push().key;
 
     // A post entry.
     var postData = {
@@ -100,16 +102,6 @@ app.controller("MainController", function($scope){
     return firebase.database().ref().update(updates);
   }
 
-  $scope.addNote = function addNote($scope) {
-
-    // Send todo to database
-    writeNewNote($scope.newNote);
-
-    // Clear form input
-    $scope.newNote = '';
-
-    return;
-  };
 
   $scope.removeNote = function removeNote($scope, key) {
 
@@ -122,12 +114,11 @@ app.controller("MainController", function($scope){
   // Watch the firebase database
   firebase.database().ref('notes/').on('value', function(snapshot) {
     $scope.notes = snapshot.val();
-    console.log("length");
-    console.log($scope.notes.length);
-    for (i = 0; i < $scope.notes.length; i++) {
-      console.log($scope.notes[i].body);
-    }
-
+    // console.log("length");
+    // console.log($scope.notes.length);
+    // for (i = 0; i < $scope.notes.length; i++) {
+    //   console.log($scope.notes[i].body);
+    // }
   });
 
   $scope.newNote = function(){
